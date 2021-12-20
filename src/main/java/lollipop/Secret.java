f9df3b8bf894a3974cbde2038530dcd41c8321b5
BooleanCube <47650058+BooleanCube@users.noreply.github.com>
2021-12-20 00:22:47 -0500

    bruh

--- src/main/java/lollipop/Secret.java
+++ src/main/java/lollipop/Secret.java
@@ -2,6 +2,6 @@ package lollipop;
 
 public class Secret {
 
-    public static String TOKEN = "OTE5MDYxNTcyNjQ5OTEwMjky.YbQUyg.rv4sfo30c3Ozp2sx-goyYvUBEJI";
+    public static String TOKEN = "never gonna give you up";
 
 }
