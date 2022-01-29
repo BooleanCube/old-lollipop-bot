53e038d8734cad02d10061f7dbbfd1813b3c60eb
BooleanCube <47650058+BooleanCube@users.noreply.github.com>
2022-01-28 20:21:18 -0500

    poop

--- src/main/java/lollipop/CONSTANT.java
+++
@@ -1,9 +0,0 @@
-package lollipop;
-
-public class CONSTANT {
-
-    public static long TESTID = 927731018151120966L;
-    public static long OWNERID = 525126007330570259L;
-    public static String PREFIX = "/";
-
-}
