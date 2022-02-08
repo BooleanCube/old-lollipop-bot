b7f437d4e3023b961ebc4351968f1cfba0f6279f
BooleanCube <47650058+BooleanCube@users.noreply.github.com>
2022-02-08 06:59:19 -0500

    permissions

--- src/main/java/lollipop/listeners/LollipopReaction.java
+++ src/main/java/lollipop/listeners/LollipopReaction.java
@@ -1,5 +1,6 @@
 package lollipop.listeners;
 
+import net.dv8tion.jda.api.Permission;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.jetbrains.annotations.NotNull;
@@ -8,7 +9,9 @@ public class LollipopReaction extends ListenerAdapter {
 
     @Override
     public void onMessageReceived(@NotNull MessageReceivedEvent event) {
-        if(event.getMessage().getContentRaw().toLowerCase().contains("lollipop"))
+        if(event.getMessage().getContentRaw().toLowerCase().contains("lollipop") && 
+                event.getGuild().getSelfMember().hasPermission(Permission.MESSAGE_ADD_REACTION) &&
+                event.getGuild().getSelfMember().hasPermission(Permission.MESSAGE_HISTORY))
             event.getMessage().addReaction("\uD83C\uDF6D").queue();
     }
 
