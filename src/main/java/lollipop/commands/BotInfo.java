5998469104971304597b579472215e2ffbe26450
BooleanCube <47650058+BooleanCube@users.noreply.github.com>
2022-02-17 00:11:48 -0500

    remove author embed

--- src/main/java/lollipop/commands/BotInfo.java
+++ src/main/java/lollipop/commands/BotInfo.java
@@ -39,7 +39,6 @@ public class BotInfo implements Command {
                 .addField("Support", "Did you encounter a bug? Join this [server](https://discord.gg/3ZDpPyR) and report in a support channel.", false)
                 .addField("Author", "**BooleanCube** (" + event.getJDA().getShardManager().getGuildById(740316079523627128l).getOwner().getUser().getAsTag() + ")\n[MyAnimeList](https://myanimelist.net/profile/BooleanCube) - [Playlist](https://open.spotify.com/playlist/4KnWT1hszQuBi4IaKdm8Pk?si=91e0fe7e73b54853) - [Discord](https://discord.gg/3ZDpPyR) - [Github](https://github.com/BooleanCube) - [Youtube](https://www.youtube.com/channel/UCsivrachJyFVLi7V60lrd6g)", false)
                 .setFooter("konnichiwa, watashi wa lollipop desu")
-                .setAuthor(event.getMember().getEffectiveName(), "https://discord.gg/3ZDpPyR", event.getMember().getEffectiveAvatarUrl())
                 .setThumbnail(event.getGuild().getSelfMember().getEffectiveAvatarUrl())
                 .build()
         ).queue();
