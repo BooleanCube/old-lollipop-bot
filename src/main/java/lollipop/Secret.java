91d24a1aa8a10082f495ff7836b0221c3383fd22
BooleanCube <47650058+BooleanCube@users.noreply.github.com>
2022-05-18 22:21:51 -0400

    noice

--- src/main/java/lollipop/Secret.java
+++ src/main/java/lollipop/Secret.java
@@ -4,5 +4,9 @@ public class Secret {
 
     public static String TOKEN = "never gonna give you up";
     public static String TESTTOKEN = "never gonna let you downnnn";
+    public static String KAWAIIAPITOKEN = "never gonna run around and desert you";
+    public static String TOPGG = "never gonna make you cry";
+    public static String DBL = "never gonna say goodbye";
+    // never gonna tell a lie and hurt you
 
 }
