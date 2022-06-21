112a1931e4648b66ceaf4d81fd48675b7e98b3ce
BooleanCube <47650058+BooleanCube@users.noreply.github.com>
2022-06-20 20:52:19 -0400

    oops

--- wiki/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"discorddb","l":"DatabaseManager"},{"p":"discorddb","l":"DatabaseObject"}];updateSearchResults();
