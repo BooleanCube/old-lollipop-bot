29adb7585f9d77d435496025b9874efc08d8aeda
boole <johnnyfastfigners1234@gmail.com>
2022-06-21 21:26:41 -0400

    v2.8.2

--- src/main/java/lollipop/Constant.java
+++ src/main/java/lollipop/Constant.java
@@ -30,6 +30,6 @@ public class Constant {
     public static double MULTIPLIER = 1.5d;
 
     // Software Version Value
-    public static String VERSION = "v2.7.8";
+    public static String VERSION = "v2.8.2";
 
 }
